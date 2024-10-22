a253e870c8e0f6375f6397b8664f40e0646d400f
jose-pc <josegu05@ucm.es>
2024-10-22 21:07:58 +0200

    edit fuses.h with a command to write

--- digital/fuses.sh
+++ digital/fuses.sh
@@ -3,3 +3,7 @@ set -x
 
 avrdude -p m328p -c arduino -P /dev/ttyACM0 -b 19200 -U lfuse:r:-:b -U hfuse:r:-:b -U efuse:r:-:b
 
+# to write fuses
+# avrdude -p m328p -c arduino -P /dev/ttyACM0 -b 19200 -U lfuse:w:0xFF:m -U hfuse:w:0xDA:m -U efuse:w:0xFD:m
+# replace hex values with hex values of choice
+
